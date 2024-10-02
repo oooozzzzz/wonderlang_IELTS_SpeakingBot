c629a8878ac9cf4b3855d68d24f26f6cb9e2f376
oooozzzzz <lobovdima27@gmail.com>
2024-10-02 20:03:06 +0300

    no create user

--- handlers/startHandler.js
+++ handlers/startHandler.js
@@ -3,9 +3,9 @@ const { createUser } = require("../db");
 const { startMenu } = require("../menus/startMenu");
 
 module.exports = async (ctx) => {
-	clearGPTContext(ctx.from.id)
-	await ctx.msg.delete()
-	await createUser(ctx.from.id, ctx.from.first_name);
-	await ctx.reply("Добро пожаловать в Wonderlang!")
-	await ctx.reply(ctx.t("start"), {reply_markup: startMenu});
+	clearGPTContext(ctx.from.id);
+	await ctx.msg.delete();
+	// await createUser(ctx.from.id, ctx.from.first_name);
+	await ctx.reply("Добро пожаловать в Wonderlang!");
+	await ctx.reply(ctx.t("start"), { reply_markup: startMenu });
 };
